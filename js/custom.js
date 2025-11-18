f997218e752b5936daa99fc7a736aa27aa951048
Hjjjkh <3480218956@qq.com>
2025-11-18 16:24:25 +0800

    Site updated: 2025-11-18 16:24:22

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,22 @@
 // 自定义 JavaScript
 
 // 等待页面加载完成后初始化 APlayer
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // 创建播放器容器
-  const metingEl = document.createElement('meting-js');
-  
+  const metingEl = document.createElement("meting-js");
+
   // 设置播放器属性
-  metingEl.setAttribute('server', 'netease');
-  metingEl.setAttribute('type', 'playlist');
-  metingEl.setAttribute('id', '2829883282');
-  metingEl.setAttribute('fixed', 'true');
-  metingEl.setAttribute('autoplay', 'false');
-  metingEl.setAttribute('loop', 'all');
-  metingEl.setAttribute('order', 'random');
-  metingEl.setAttribute('preload', 'auto');
-  metingEl.setAttribute('list-folded', 'true');
-  metingEl.setAttribute('lrc-type', '3');
-  
+  metingEl.setAttribute("server", "netease");
+  metingEl.setAttribute("type", "playlist");
+  metingEl.setAttribute("id", "2829883282");
+  metingEl.setAttribute("fixed", "true");
+  metingEl.setAttribute("autoplay", "false");
+  metingEl.setAttribute("loop", "all");
+  metingEl.setAttribute("order", "random");
+  metingEl.setAttribute("preload", "auto");
+  metingEl.setAttribute("list-folded", "true");
+  metingEl.setAttribute("lrc-type", "3");
+
   // 将播放器添加到页面
   document.body.appendChild(metingEl);
 });
-
